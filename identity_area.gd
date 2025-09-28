254757511bb38b02ce9447070e6768f4b4b2e78f
Saisanthosh-Selvakumar <sai.primary.12@gmail.com>
2025-09-28 05:32:59 -0400

    Add files via upload

--- identity_area.gd
+++ identity_area.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
 signal player_near(area_name)
-const KEY_TEXTURE = preload("res://")
-#MENTION ANIMATED IMAGE HERE
-@export var area_name = "identity"  # Change to "identity" or "joy" in each area
 
+@export var area_name = "identity"  # Change to "identity" or "joy" in each area
+@export var limit_left: int
+@export var limit_right: int
+@export var limit_top: int
+@export var limit_bottom: int
 func _on_body_entered(body):
-	if body.name == "Player":
-		for sprite in get_tree().get_nodes_in_group("changeable_sprites"):
-			if sprite is Sprite2D:
-				sprite.texture = KEY_TEXTURE
+		
+		if body.name == "Player":
+			
+			if body.selection == "none" or body.selection == "identity":
+				var camera = body.get_node("Camera2D")
+				camera.limit_left = limit_left - 100
+				camera.limit_right = limit_right - 100
+				camera.limit_top = limit_top - 100
+				camera.limit_bottom = limit_bottom - 100
+				body.selection = "identity"
+				print(body.selection)
