919e29f6023898721b185447db9bb78fc5c5ff2d
Piepancake <113853867+Piepancake@users.noreply.github.com>
2025-09-28 04:34:10 -0500

    Add files via upload

--- hope_area.gd
+++ hope_area.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
-#signal player_near(area_name)
+signal player_near(area_name)
 
-#@export var limit_left: int
-#@export var limit_right: int
-#@export var limit_top: int
-#@export var limit_bottom: int
+
 
 @export var area_name = "hope"  # Change to "identity" or "joy" in each area
 
@@ -14,11 +11,7 @@ func _on_body_entered(body):
 		
 		if body.name == "Player":
 			if body.selection == "none" or body.selection == "hope":
-				#var camera = body.get_node("Camera2D")
-				#camera.limit_left = limit_left
-				#camera.limit_right = limit_right
-				#camera.limit_top = limit_top
-				#camera.limit_bottom = limit_bottom
+				
 				body.selection = "hope"
 				
 				
--- player.gd
+++ player.gd
@@ -5,12 +5,7 @@ var JUMP_VELOCITY = -300.0
 @export var jump_delay = 0.0
 var jumping = false
 @export var selection = "none"
-@onready var vignette = get_node("../Vignette/Vignette")
 
-func _ready() -> void:
-	# start hidden
-	vignette.visible = false
-	vignette.modulate.a = 0.0
 
 func apply_jet_force(force: Vector2) -> void:
 	print("Jet force applied")
@@ -25,6 +20,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor() and not jumping:
 		jumping = true
 		jump_with_delay()
+	
+	
+		
+		
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,6 +35,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	move_and_slide()
+
+	
 
 func jump_with_delay() -> void:
 	await get_tree().create_timer(jump_delay).timeout
@@ -58,11 +60,3 @@ func _process(_delta):
 		_animated_sprite.speed_scale = walk_speed_scale
 	else:
 		_animated_sprite.stop()
-
-func _on_hope_area_body_entered(body: Node) -> void:
-	# ensure it's the player (body will be the entering body)
-	if body == self:
-		vignette.visible = true
-		vignette.modulate.a = 0.0
-		# fade in (0.4s)
-		create_tween().tween_property(vignette, "modulate:a", 1.0, 0.4)
